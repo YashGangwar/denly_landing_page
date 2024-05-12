2815ed6570363c6ca03cc33c27373e66f3f84249
Yash Gangwar <yashgangwar007@gmail.com>
2024-05-12 20:39:55 +0530

    Config asset change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import icon from 'astro-icon';
 
 const build = {
   baseURL: '/',
+  outDir: './dist',
   siteURL: 'http://localhost:4321/'
 }
 
@@ -11,12 +12,12 @@ const env = process.env.NODE_ENV
 
 switch (env) {
   case 'pages':
-    build.baseURL = '/denly-landing-page/'
+    build.baseURL = '/'
     build.siteURL = 'https://yashgangwar.github.io'
     break
 
   case 'production':
-    build.baseURL = '/denly-landing-page/'
+    build.baseURL = '/'
     build.siteURL = 'https://yashgangwar.github.io'
     break
 
@@ -28,10 +29,11 @@ switch (env) {
 export default defineConfig({
   site: build.siteURL,
   base: build.baseURL,
+  outDir: build.outDir,
   integrations: [
+    icon(),
     tailwind({
-      applyBaseStyles: true
-    }),
-    icon()
+      applyBaseStyles: false
+    })
   ]
 })
